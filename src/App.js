9099c011e7d61295030aa46fe3dfa574e5c1b004
KDEVCORE <kdevcore@gmail.com>
2023-04-06 18:47:55 +0900

    application main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
+import { AppBar, Box, Button, CircularProgress, Container, Grid, List, Paper, Toolbar, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import AddTodo from "./pages/todo/AddTodo";
+import Todo from "./pages/todo/Todo";
+import { call, signOut } from "./service/ApiService";
+
 function App() {
-  return (
-    <div>
-      <h1>TEST only</h1>
-      <h4>- HTTPS</h4>
-      <h4>- React</h4>
-    </div>
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    call("/todo", "GET", null).then((response) => {
+      setItems(response.data);
+      setLoading(false);
+    });
+  }, []);
+
+  const addItem = (item) => {
+    call("/todo", "POST", item)
+    .then((response) => setItems(response.data));
+  };
+
+  const editItem = (item) => {
+    call("/todo", "PUT", item)
+    .then((response) => setItems(response.data));
+  };
+
+  const deleteItem = (item) => {
+    call("/todo", "DELETE", item)
+    .then((response) => setItems(response.data));
+  };
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo
+            item={item}
+            key={item.id}
+            editItem={editItem}
+            deleteItem={deleteItem}
+          />
+        ))}
+      </List>
+    </Paper>
   );
+
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">MAIN</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" onClick={signOut}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
+  let todoListPage = (
+    <Box maxWidth="md">
+      {navigationBar}
+      <AddTodo addItem={addItem} />
+      {todoItems}
+    </Box>
+  );
+
+  let loadingPage = (
+    <Box margin={5} textAlign={"center"}>
+      <CircularProgress size={150} />
+    </Box>
+  );
+
+  return <Container>{!loading ? todoListPage : loadingPage}</Container>;
 }
 
 export default App;
