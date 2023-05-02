a8b723b327b24f9d9d8ce49dd8323d740bfa3d7d
KDEVCORE <kdevcore@gmail.com>
2023-05-02 19:25:40 +0900

    about me update

---
+++ src/pages/About.js
@@ -0,0 +1,164 @@
+import { Box, Button, Card, CardContent, CardHeader, Chip, Paper, Stack, Step, StepContent, StepLabel, Stepper, Tab, Tabs, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <Paper
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography component="span">{children}</Typography>
+                </Box>
+            )}
+        </Paper>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `tab-${index}`,
+        'aria-controls': `tabpanel-${index}`,
+    };
+}
+
+const steps = [
+    {
+        label: "가치관",
+        title: "처음부터 완벽할 수 없으므로, 리뷰(Review)와 리팩토링(Refactoring)으로 좋은 개발자가 된다.",
+        description: "SI/SM 업무경험에서 느꼈던 개발 환경에 의한 유연하지 못한 시스템을 경험하면서, 실행만 되는 로직이 아닌 효율적인 코드 작성의 중요성을 깨달았습니다.",
+    },
+    {
+        label: "목표",
+        title: "성장하고 싶은 개발자",
+        description: "Spring Legacy Project로 Web 개발을 시작했지만, 개발자는 새로운 기술 습득이 중요하다고 생각합니다. 최근에는 Spring Boot, JPA를 사용하여 웹 개발환경도 경험했고, 그것을 바탕으로 Spring Security, JWT, OAuth 2를 주입해서 포트폴리오 개발, AWS에 배포하여 도메인 발급 및 https를 적용했습니다.",
+    },
+    {
+        label: "기술 경험",
+        title: "Java 기반 Spring Framework 웹 개발",
+        description: "분석, 설계, 배포에 대한 경험은 다소 부족하지만, 개발자의 기본기라고 생각하는 구현 경험은 다양합니다.",
+    },
+];
+
+
+export default function About() {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [activeStep, setActiveStep] = useState(0);
+    const [chipData] = useState([
+        { key: 0, label: 'Java' },
+        { key: 1, label: 'Spring Framework' },
+        { key: 2, label: 'Spring Boot' },
+        { key: 3, label: 'Spring Security' },
+        { key: 4, label: 'REST API' },
+        { key: 5, label: 'JPA' },
+        { key: 6, label: 'JWT' },
+        { key: 7, label: 'OAuth 2' },
+        { key: 8, label: 'JavaScript' },
+        { key: 9, label: 'jQuery' },
+        { key: 10, label: 'React' },
+        { key: 11, label: 'GitHub' },
+        { key: 12, label: 'AWS' },
+    ]);
+
+    const handleTabChange = (event, swapIndex) => {
+        setTabIndex(swapIndex);
+    };
+
+    const handleNextStep = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBackStep = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepReset = () => {
+        setActiveStep(0);
+    };
+
+    return (
+        <Paper sx={{ width: "50%", justifyContent: "center" }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={tabIndex} onChange={handleTabChange}>
+                    <Tab label="me" {...a11yProps(0)} />
+                    <Tab label="contact" {...a11yProps(1)} />
+                </Tabs>
+            </Box>
+            <TabPanel value={tabIndex} index={0}>
+                <Stepper activeStep={activeStep} orientation="vertical">
+                    {steps.map((step, index) => (
+                        <Step key={index}>
+                            <StepLabel>
+                                {step.label}
+                            </StepLabel>
+                            <StepContent>
+                                <Card>
+                                    <CardHeader title={step.title} />
+                                    <CardContent>
+                                        {step.description}
+                                    </CardContent>
+                                    {index === steps.length - 1 && (
+                                        <CardContent>
+                                            <Stack direction="row" spacing={1}>
+                                                {chipData.map((data) => {
+                                                    return (
+                                                        <Chip key={data.key} label={data.label} variant="outlined" />
+                                                    );
+                                                })}
+                                            </Stack>
+                                        </CardContent>
+                                    )}
+                                </Card>
+                                <Box sx={{ mb: 2 }}>
+                                    <div>
+                                        <Button
+                                            variant="contained"
+                                            onClick={handleNextStep}
+                                            sx={{ mt: 1, mr: 1 }}
+                                            >
+                                            {index === steps.length - 1 ? 'finish' : 'next'}
+                                        </Button>
+                                        <Button
+                                            variant="contained"
+                                            disabled={index === 0}
+                                            onClick={handleBackStep}
+                                            sx={{ mt: 1, mr: 1 }}
+                                        >
+                                            {"back"}
+                                        </Button>
+                                    </div>
+                                </Box>
+                            </StepContent>
+                        </Step>
+                    ))}
+                </Stepper>
+                {activeStep === steps.length && (
+                    <Paper square elevation={0} sx={{ p: 3 }}>
+                        <Box>
+                            <Typography component="span">{"감사합니다.😄"}</Typography>
+                        </Box>
+                        <Button variant="outlined" onClick={handleStepReset} sx={{ mt: 1, mr: 1 }}>
+                            {"처음부터 다시 보기"}
+                        </Button>
+                    </Paper>
+                )}
+            </TabPanel>
+            <TabPanel value={tabIndex} index={1}>
+                Item Two
+            </TabPanel>
+        </Paper>
+    );
+}
